ab38baa666cb96ef7a77884117e1c13bf4f7eb15
BraianMelo <Braian.ptu@gmail.com>
2025-03-03 20:30:25 -0300

    Payment, one-to-one association

--- src/main/java/com/braian/curso/config/TestConfig.java
+++ src/main/java/com/braian/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.braian.curso.entities.Category;
 import com.braian.curso.entities.Order;
 import com.braian.curso.entities.OrderItem;
+import com.braian.curso.entities.Payment;
 import com.braian.curso.entities.Product;
 import com.braian.curso.entities.User;
 import com.braian.curso.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2024-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 		
